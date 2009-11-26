6e510c082fb2147f3f03e73c75dee5906241f23c
John Evans <john@milo.com>
2009-11-25 19:24:51 -0800

    Fixed up some more stuff.

--- nyx.milo.com.el
+++ nyx.milo.com.el
@@ -15,3 +15,18 @@
                '("\\.py\\'" flymake-pylint-init)))
 
 (add-hook 'python-mode-hook (lambda() (flymake-mode t)))
+
+
+;; Erlang
+;;(setq load-path (cons  "/usr/share/emacs22/site-lisp/erlang/erlang"
+;;                       load-path))
+;;(setq erlang-root-dir "/usr")
+;;(setq exec-path (cons "/usr/bin" exec-path))
+(require 'erlang-start)
+
+;;(setq ipython-command "/pluto/local/bin/ipython")
+;;(load "~/.emacs.d/vendor/ipython.el")
+;;(require 'ipython)
+
+(load "~/go/misc/emacs/go-mode.el")
+
